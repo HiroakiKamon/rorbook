b33fa580625061b408f77f8a136660107760714d
Kamon <Hiroaki@Hiroaki-MacBook-Pro.local>
2014-07-31 14:12:47 +0900

    fixture books

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140731050639) do
+
+  create_table "books", force: true do |t|
+    t.string   "isbn"
+    t.string   "title"
+    t.integer  "price"
+    t.string   "publish"
+    t.date     "published"
+    t.boolean  "cd"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
