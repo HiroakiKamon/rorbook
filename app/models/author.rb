b5dc710f27ec48602ed8d9dad96e3a59674c2fb4
Kamon <Hiroaki@Hiroaki-MacBook-Pro.local>
2014-08-01 11:05:20 +0900

    a

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :books
+  has_many :comments , -> { where(deleted:fase)}, class_name: 'FanComment',
+    foreign_key: 'author_no'
 end
